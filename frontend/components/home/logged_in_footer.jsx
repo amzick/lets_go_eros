e05b0119651173736f11a0b46e795660f7544e1b
amzick <aaron.zick@gmail.com>
2019-02-18 12:18:37 -0500

    Add Personal Link To Footers

--- frontend/components/home/logged_in_footer.jsx
+++ frontend/components/home/logged_in_footer.jsx
@@ -5,6 +5,7 @@ const LoggedInFooter = (props) => {
     <footer className="logged-in-footer">
       <div className="logged-in-footer-div">
         <span>Replica of OKCupid Built By Aaron Zick. Hire me!</span>
+        <span><a target="_blank" href="https://www.aaronzick.com"><i class="fas fa-laptop"></i></a></span>
         <span><a target="_blank" href="https://www.linkedin.com/in/amzick/"><i className="fab fa-linkedin"></i></a></span>
         <span><a target="_blank" href="https://github.com/amzick"><i className="fab fa-github"></i></a></span>
         <span><a target="_blank" href="https://angel.co/aaron-zick?public_profile=1"><i className="fab fa-angellist"></i></a></span>
